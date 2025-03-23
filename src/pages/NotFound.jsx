b899a36d2bced5c837dbd5cb9db4bf866446ad6e
Alvul07 <tolonovalihan007@gmail.com>
2025-03-23 13:15:13 +0600

    NotFound done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
-        
+    <div className='text-[100px] flex flex-col items-center mt-[100px]'>
+        <h1>404</h1>
+        <h2>Not Found</h2>
     </div>
   )
 }
