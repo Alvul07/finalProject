a048dba075d999ee9b519a4cd6adc541bca48917
Alvul07 <tolonovalihan007@gmail.com>
2025-03-23 12:12:59 +0600

    change in SectionTwo

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -3,6 +3,7 @@ import WhiteBtn from './WhiteBtn'
 import ArrowR from '../assets/arrowR.svg'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import ApiProps from './ApiProps'
 
 
 const SectionTwo = () => {
@@ -23,19 +24,9 @@ const SectionTwo = () => {
         <section className='max-w-[1110px] mx-auto mt-[130px]'>
             <h1 className='font-[500] text-[40px] mb-[92px]'>Новая коллекция</h1>
             <div className='flex justify-between'>
-                {cards.slice(0, 3).map((item) => (
+                {cards.slice(0, 3).map((item, index) => (
                     <div className='flex gap-[30px]'>
-                        <div>
-                            <button className='duration-500 relative group cursor-pointer overflow-hidden'>
-                                <img className='w-[350px] h-[478px] object-cover' src={item.img} alt="" />
-                                <div className="absolute inset-0 bg-[#6E9C9FA3] opacity-0 group-hover:opacity-75 transition-opacity duration-500"></div>
-                                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                                    <span><img src={ArrowR} alt="" /></span>
-                                </div>
-                            </button>
-                            <h4 className='text-center text-[20px] font-[500] mt-[20px]'>{item.title}</h4>
-                            <p className='text-[#998E78] text-center text-[15px] font-[500]'>{item.price}</p>
-                        </div>
+                        <ApiProps key={index} img={item.img} title={item.title} price={item.price}/>
                     </div>
                 ))}
             </div>
