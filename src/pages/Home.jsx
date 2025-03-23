5dfeab017db645ae887a717d971d319e3f0113b0
Alvul07 <tolonovalihan007@gmail.com>
2025-03-23 13:18:36 +0600

    removed no need

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,26 +7,9 @@ import SectionFour from '../components/SectionFour';
 import Footer from '../components/Footer';
 
 const Home = () => {
-  // const [cars, setCars] = useState([]);
-
-  // useEffect(() => {
-  //     axios.get("https://90bd69b27d9ba951.mokky.dev/cars")
-  //     .then((response) => {
-  //         setCars(response.data)
-  //     })
-  //     .catch((error) => {
-  //         console.log(error);
-  //     });
-  // }, []);
 
   return (
     <div>
-      {/* <h1>Список машин</h1>
-        {cars.map((item, index) => (
-            <h3 className='text-[teal]' key={index}>{item.title}
-            <span>Цена: {item.price}</span>
-            </h3>
-        ))} */}
       <div className='flex justify-end'>
         <div className='bg-[#F1EADC] w-[620px] h-[826px] absolute top-0'></div>
       </div>
